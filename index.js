161879b309757229713b503486582311cb3add34
R8152 <raulteles05@gmail.com>
2026-05-07 21:51:02 -0300

    Começando JavaScript

---
+++ index.js
@@ -0,0 +1,25 @@
+// Métodos de Saída
+alert("Hello World!")
+// Métodos de Entrada
+confirm("Deseja receber notificações?")
+
+//Tipos de Dados
+
+"Raul" - String
+// Em JavaScript, ambos números inteiro e flutuante são Number
+12 - Number
+// undefined é um valor declarado, mas não definido
+undefined
+// null é um valor que não traz resposta alguma
+null
+// NaN - Not a Number
+// True/False - Boolean
+
+// Variáveis
+
+// Posso declarar e atribuir valor depois
+let turma = "3C"
+
+turma = "2E"
+
+const pessoa //Erro: Necessário atribuir valor
