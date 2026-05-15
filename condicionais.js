30cfc9ef83ba809133a5a46270938d70adfd8883
R8152 <raulteles05@gmail.com>
2026-05-14 22:04:33 -0300

    Aula - 14/05/26

---
+++ condicionais.js
@@ -0,0 +1,18 @@
+if (condicao){
+    //Bloco de condição verdadeiro
+} else{
+    //Bloco de condição falso
+}
+
+// const idade = prompt("Digite sua idade: ")
+
+// if (idade >= 18){
+//     alert("Você poderá tirar carteira de habilitação apenas a partir de 18 anos.")
+// }
+
+// const habilitacao = prompt("Você possui habilitação?")
+// if (idade >= 18 && habilitacao.toUpperCase() === 'SIM'){
+//     console.log('Pode dirigir')
+// } else{
+//     console.log('Não pode dirigir')
+// }
--- index.js
+++ index.js
@@ -1,25 +1,78 @@
-// Métodos de Saída
-alert("Hello World!")
-// Métodos de Entrada
-confirm("Deseja receber notificações?")
+// // Métodos de Saída
+// alert("Hello World!")
+// // Métodos de Entrada
+// confirm("Deseja receber notificações?")
+// const notificacao = confirm("Deseja receber notificações?")
+// console.log(notificacao)
+// const nome = prompt('Digit seu nome: ') //prompt recebe dado em string
 
-//Tipos de Dados
+// // Conversões
 
-"Raul" - String
-// Em JavaScript, ambos números inteiro e flutuante são Number
-12 - Number
-// undefined é um valor declarado, mas não definido
-undefined
-// null é um valor que não traz resposta alguma
-null
-// NaN - Not a Number
-// True/False - Boolean
+// const idade = Number("46")
+// const num1 = parseInt("32.7") //32
+// const num2 = parseFloat("38.6").toFixed(2) //38.60
+// const num3 = parseFloat(prompt("Digite outro número: ")).toFixed(2)
 
-// Variáveis
+// const cpf = Number(prompt(12378239567))
+// //Tipos de Dados
 
-// Posso declarar e atribuir valor depois
-let turma = "3C"
+// "Raul" - String
+// // Em JavaScript, ambos números inteiro e flutuante são Number
+// 12 - Number
+// // undefined é um valor declarado, mas não definido
+// undefined
+// // null é um valor que não traz resposta alguma
+// null
+// // NaN - Not a Number
+// // True/False - Boolean
 
-turma = "2E"
+// // Variáveis
 
-const pessoa //Erro: Necessário atribuir valor
+// // Posso declarar e atribuir valor depois
+// let turma = "3C"
+
+// turma = "2E"
+
+// // const pessoa //Erro: Necessário atribuir valor
+
+// // Operadores
+// //
+let t1 = 9;
+t1++
+console.log(t1)
+t1--
+console.log(t1)
+let t2 = 10;
+t2 += 10
+console.log(t2)
+t2 -= 6
+console.log(t2)
+t2 *= 8
+console.log(t2)
+t2 /= 2
+console.log(t2)
+
+// Qual o resultado da seguinte expressão?
+let total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+// Crie duas expressões que retornem NaN
+let n1 = 0/0
+console.log(n1)
+let n2 = 4-"r"
+console.log(n2)
+// Somar a string '200' com o número 50 e retornar 250
+let s = Number("200")
+let n = 50
+let soma = s + n
+console.log(soma)
+// Incremente o número 5 e retorne o seu valor incrementado
+let ni = 5
+ni++
+console.log(ni)
+// Como dividir o peso por 2? 
+let numero = '60';
+let unidade = 'kg';
+numero = Number(numero)/2
+let peso = numero + unidade;
+console.log(peso)
